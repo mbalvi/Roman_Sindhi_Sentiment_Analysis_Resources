c11d45671cb088595963dea8834851959e34422c
Muhammad Bux Alvi <engralvi@gmail.com>
2024-05-12 14:04:45 +0500

    Add files via upload

---
+++ RS_word_SC.py
@@ -0,0 +1,100 @@
+'aadi' : -0.4
+'laanat' :-0.7
+'aajzi waaro' : 0.3
+'madad' : 0.4
+'aazad khyaal' : 0.0
+'madadgaar' : 0.4
+'achraj' : 0.8
+'malaamat' : -0.4
+'afsos' : -0.3
+'maloomati' : 0.5
+'agraee' : -0.4
+'masa'ala' : -0.4
+'agwaan' : 0.6
+'mazboot' : 0.43
+'akelo' : -0.4
+'mehnti' : 0.7
+'azaab' : -0.2
+'munkasir' : 0.2
+'baa-ikhlaaq' : 0.5
+'nasl' : 0.0
+'bahkn' : 0.8
+'nemat' : 0.6
+'baley baley' : 0.8
+'nojawaan' : 0.0
+'barbaad' : -0.4
+'nojawaan nasl' : 0.0
+bdnaam' : -0.5
+'ootaak' : 0.0
+'beemaar' : -0.5
+'pako sathee' : 0.6
+'bethak' : 0.0
+'mayousi pasand' : -0.6
+'beywaah' : -0.4
+'preshani' : -0.4
+'bhalae' : 0.6
+'pur kashish' : 0.4
+'bigaarr' : -0.4
+'pur-josh' : 0.375
+'chaalak' : -0.3
+'qaboolyat' : 0.2
+'changaee' : 0.6
+'qadeem' : 0.1
+'dhokhe baaz' : -0.8
+'ruthal' : -0.5
+'dhokho' : -0.8
+'sahap waaro': 0.5
+'dhoko' : -0.6
+'sakhee' : 0.6
+'faedemand' : 0.4
+'sakhi' : 0.6
+'faedo' : 0.4
+'sakhtgeer' : 0.0
+'fahash' : -0.6
+'sarshaar' : 0.4
+'freb' : -0.6
+'shaeq' : 0.3
+'frmaanbardaar' : 0.3
+'sharmnaak' : -0.5
+'gadlo' : -0.4
+'shokeen' : 0.4
+'gair mutma’in' : -0.5
+'sutho' : 0.6
+'gehro' : -0.4
+'ta’awun knder' : 0.4
+'gumm' : -0.6
+'tafreeh' : 0.6
+'hamlo' : -0.2
+'tanked' : -0.4
+'hathee' : 0.4
+'tanz' : -0.4
+'himat deendar' : 0.4
+'tarakee' : 0.5
+'himthaen' : 0.4
+'tareef' : 0.6
+'ihtjaaj' : 0.0
+'tbaah' : -0.6
+'ikhlaak' : 0.0
+'tez raftaar' : 0.0
+'ilzaam' : -0.4
+'tez taraar' : -0.3
+'istemaal' : 0.0
+'tuhmat' : -0.4
+'izafo krn' : 0.2
+'vaadh' : 0.2
+'jaakhoree' : 0.7
+'waddo' : 0.2
+'jaan nisaar' : 0.6
+'wadhaaro' : 0.2
+'josheelo' : 0.375
+'wadhao' : 0.4
+'kaawar' : -1
+'wah wa' : 1
+'khoobsoorat' : 0.85
+'waqf' : 0.4
+'khuwaar' : -0.5
+'winjaayal' : -0.6
+'kno' : -0.6
+'wqt jo paaband' : 0.6
+'kushaada dil' : 0.3
+'ziyaan' : -0.6
